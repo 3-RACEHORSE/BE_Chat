668b7ad171550a7d4f7c1dab99edccf2c38d2366
hyun7494 <et009153@gmail.com>
2024-05-11 14:34:47 +0900

    fix: Response 수정, Entity 수정, roomNumber uuid 활용

--- src/main/java/com/skyhorsemanpower/chatService/chat/application/ChatServiceImp.java
+++ src/main/java/com/skyhorsemanpower/chatService/chat/application/ChatServiceImp.java
@@ -3,6 +3,8 @@ package com.skyhorsemanpower.chatService.chat.application;
 import com.skyhorsemanpower.chatService.chat.data.dto.ChatMemberDto;
 import com.skyhorsemanpower.chatService.chat.domain.ChatRoom;
 import com.skyhorsemanpower.chatService.chat.infrastructure.ChatRoomRepository;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,20 +13,28 @@ import org.springframework.stereotype.Service;
 public class ChatServiceImp implements ChatService{
     private final ChatRoomRepository chatRoomRepository;
     @Override
-    public boolean createChatRoom(ChatMemberDto chatMemberDto) {
-        ChatRoom chatRoom = ChatRoom.builder()
-            .chatMember1(chatMemberDto.getFirstMemberUuid())
-            .chatMember2(chatMemberDto.getSecondMemberUuid())
-//            .createdAt() // createdAt 얘네들은 옛날에는 상속받아서 했는데
-            .lastChat(null)
-//            .lastChatCreatedAt() // 얘도
-            .build();
+    public boolean createChatRoom(List<ChatMemberDto> chatMemberDtos) {
+        if (chatMemberDtos.size() < 2) {
+            throw new IllegalArgumentException("최소 2명의 회원이 필요합니다.");
+        }
+
         try {
-            chatRoomRepository.save(chatRoom);
+            String roomNumber = UUID.randomUUID().toString();
+
+            for (ChatMemberDto chatMemberDto : chatMemberDtos) { // 리스트에서 꺼내어 반복
+                String userUuid = chatMemberDto.getMemberUuid();
+
+                ChatRoom chatRoom = ChatRoom.builder()
+                    .roomNumber(roomNumber)
+                    .memberUuid(userUuid)
+                    .build();
+
+                chatRoomRepository.save(chatRoom);
+            }
+
             return true;
         } catch (Exception e) {
             return false;
         }
-
     }
 }
--- src/main/java/com/skyhorsemanpower/chatService/chat/presentation/ChatController.java
+++ src/main/java/com/skyhorsemanpower/chatService/chat/presentation/ChatController.java
@@ -3,7 +3,12 @@ package com.skyhorsemanpower.chatService.chat.presentation;
 import com.skyhorsemanpower.chatService.chat.application.ChatService;
 import com.skyhorsemanpower.chatService.chat.data.dto.ChatMemberDto;
 import com.skyhorsemanpower.chatService.chat.data.vo.AddChatRoomRequestVo;
+import com.skyhorsemanpower.chatService.common.ExceptionResponse;
+import com.skyhorsemanpower.chatService.common.ResponseStatus;
+import com.skyhorsemanpower.chatService.common.SuccessResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +21,13 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping("/room")
-    public String addChatRoom(@RequestBody AddChatRoomRequestVo addChatRoomRequestVo) {
-        ChatMemberDto chatMemberDto = addChatRoomRequestVo.toChatMemberDto();
-        if (chatService.createChatRoom(chatMemberDto)) {
-            return "성공";
+    public ResponseEntity<?> addChatRoom(@RequestBody AddChatRoomRequestVo addChatRoomRequestVo) {
+        List<ChatMemberDto> chatMemberDtos = addChatRoomRequestVo.toChatMemberDto();
+        boolean result = chatService.createChatRoom(chatMemberDtos);
+        if (result) {
+            return new SuccessResponse<>("요청 성공");
         } else {
-            return "실패";
+            return new ExceptionResponse(ResponseStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
