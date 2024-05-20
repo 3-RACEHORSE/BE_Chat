8be6d0404a8cbade52d0de7ce05472689fa8d33e
hyun7494 <et009153@gmail.com>
2024-05-20 12:43:12 +0900

    fix: commit에서 yml파일 지우기

--- src/main/java/com/skyhorsemanpower/chatService/common/ResponseStatus.java
+++ src/main/java/com/skyhorsemanpower/chatService/common/ResponseStatus.java
@@ -11,15 +11,7 @@ public enum ResponseStatus {
      * 200: 요청 성공
      **/
     SUCCESS(200, "요청에 성공했습니다."),
-    //
-//    /**
-//     * 400 : security 에러
-//     */
-//    WRONG_JWT_TOKEN(false, 401, "다시 로그인 해주세요"),
-//
-//    /**
-//     * 900: 기타 에러
-//     */
+
     NOT_ENOUGH_MEMBERS(400, "최소 두명 이상이 있어야합니다."),
     INTERNAL_SERVER_ERROR(500, "서버 오류 발생"),
     SAVE_CHAT_FAILED(500, "채팅 저장 실패"),
@@ -30,5 +22,4 @@ public enum ResponseStatus {
 
     private final int code;
     private final String message;
-
 }
