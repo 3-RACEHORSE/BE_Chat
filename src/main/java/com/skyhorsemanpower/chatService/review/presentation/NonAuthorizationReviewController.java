6f6cc8fe55d8e22e92a68fe8263c3740b02f6484
hyun7494 <et009153@gmail.com>
2024-05-22 10:45:45 +0900

    feat: 리뷰 controller 생성

---
+++ src/main/java/com/skyhorsemanpower/chatService/review/presentation/NonAuthorizationReviewController.java
@@ -0,0 +1,16 @@
+package com.skyhorsemanpower.chatService.review.presentation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/non-authorization/review")
+@Tag(name = "인가가 필요없는 리뷰", description = "인가가 필요없는 리뷰 관련 API")
+@Slf4j
+public class NonAuthorizationReviewController {
+
+}
