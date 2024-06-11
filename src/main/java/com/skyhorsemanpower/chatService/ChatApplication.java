aae54244da19b07aff115d8994385a4331ee9d0f
hyun7494 <et009153@gmail.com>
2024-06-11 11:34:26 +0900

    fix: 빠진 부분 수정

--- src/main/java/com/skyhorsemanpower/chatService/ChatApplication.java
+++ src/main/java/com/skyhorsemanpower/chatService/ChatApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @RefreshScope
 @EnableDiscoveryClient
