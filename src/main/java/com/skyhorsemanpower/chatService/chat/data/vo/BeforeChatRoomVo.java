288ea8e107ec5877570678c0bcebc152599003b1
hyun7494 <et009153@gmail.com>
2024-06-13 18:11:41 +0900

    feat: kafka 설정

---
+++ src/main/java/com/skyhorsemanpower/chatService/chat/data/vo/BeforeChatRoomVo.java
@@ -0,0 +1,20 @@
+package com.skyhorsemanpower.chatService.chat.data.vo;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@ToString
+public class BeforeChatRoomVo {
+    private String auctionUuid;
+    private List<String> memberUuids;
+    @Builder
+    public BeforeChatRoomVo(String auctionUuid, List<String> memberUuids) {
+        this.auctionUuid = auctionUuid;
+        this.memberUuids = memberUuids;
+    }
+}
---
+++ src/main/java/com/skyhorsemanpower/chatService/common/kafka/KafkaConsumerCluster.java
@@ -0,0 +1,34 @@
+package com.skyhorsemanpower.chatService.common.kafka;
+import com.skyhorsemanpower.chatService.chat.application.ChatService;
+import com.skyhorsemanpower.chatService.chat.data.vo.BeforeChatRoomVo;
+import java.util.LinkedHashMap;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaConsumerCluster {
+
+    private final ChatService chatService;
+
+    @KafkaListener(topics = "payment-close-topic", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(@Payload LinkedHashMap<String, Object> message,
+        @Headers MessageHeaders messageHeaders) {
+        log.info("consumer: success >>> message: {}, headers: {}", message.toString(),
+            messageHeaders);
+        //message를 PaymentReadyVo로 변환
+        BeforeChatRoomVo beforeChatRoomVo = BeforeChatRoomVo.builder()
+            .auctionUuid(message.get("auctionUuid").toString())
+            .memberUuids((List<String>) message.get("memberUuids"))
+            .build();
+        log.info("auctionUuid : {}", beforeChatRoomVo.getAuctionUuid());
+        log.info("memberUuids : {}", beforeChatRoomVo.getMemberUuids());
+    }
+}
